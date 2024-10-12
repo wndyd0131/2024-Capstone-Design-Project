81c99044192251d4e7add195bc15237937412c76
applepops <jihee336723@gmail.com>
2024-10-13 04:53:24 +0900

    frontend demo ui: login, chat interface

---
+++ perfectsm/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import AppRouter from "./Router"; // Router.jsx를 가져옴
+
+function App() {
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
