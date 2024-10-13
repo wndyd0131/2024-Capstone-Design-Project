44cd95843d67400aeab90fce8ab662bf26bf1c9e
applepops <jihee336723@gmail.com>
2024-10-13 23:28:26 +0900

    revise page link

--- perfectsm/src/Router.jsx
+++ perfectsm/src/Router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { useState } from "react";
 import LoginScreen from "./login"; // login page
 import ChatInterface from "./chat-interface"; // chat interface page
@@ -9,14 +9,26 @@ export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         {/* 로그인 상태에 따라 라우팅 결정 */}
         <Route
-          path="/"
+          path="/login"
           element={
             isLoggedIn ? (
-              <ChatInterface /> // 로그인이 성공하면 ChatInterface로 이동
+              <Navigate to="/chatinterface" /> // 로그인이 성공하면 ChatInterface로 이동
             ) : (
               <LoginScreen setIsLoggedIn={setIsLoggedIn} /> // 로그인 상태가 아니면 로그인 페이지
+            )
+          }
+        />
+        {/* /chatinterface 경로 추가 - 로그인 상태일 때만 ChatInterface 렌더링 */}
+        <Route
+          path="/chatinterface"
+          element={
+            isLoggedIn ? (
+              <ChatInterface /> // 로그인 상태일 때만 채팅 인터페이스 화면을 렌더링
+            ) : (
+              <Navigate to="/login" /> // 로그인하지 않은 상태라면 /login으로 리디렉션
             )
           }
         />
