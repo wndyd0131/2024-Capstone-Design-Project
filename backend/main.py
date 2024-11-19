73cd7742de6570628e82a96e9e7cf6fbd17831aa
wndyd <wndyd0131@naver.com>
2024-11-20 01:29:05 +0900

    .

--- backend/main.py
+++ backend/main.py
@@ -6,11 +6,11 @@ from backend.db.session import engine, Base
 
 app = FastAPI()
 
-@app.on_event("startup")
-async def on_startup():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
+# @app.on_event("startup")
+# async def on_startup():
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.drop_all)
+#         await conn.run_sync(Base.metadata.create_all)
 
 app.add_middleware(
     CORSMiddleware,
