930512d996ad15a5b4cccc3f126bf9f39d9be2a3
wndyd <wndyd0131@naver.com>
2024-11-19 19:07:52 +0900

    fixed find_user api

--- backend/api/user.py
+++ backend/api/user.py
@@ -18,26 +18,15 @@ from backend.utils.mail_config import conf
 
 router = APIRouter()
 
-@router.get("/", response_model=List[UserResponse], tags=["user"])
-async def find_users(db: AsyncSession = Depends(get_db)): # GET USERS
-    result = await db.execute(select(User))
-    users = result.scalars().all()
-    return users
-
-@router.get("/{user_id}", response_model=UserResponse, tags=["user"])
+@router.get("/", response_model=UserResponse, tags=["user"])
 async def find_user(
-        user_id: int,
         db: AsyncSession = Depends(get_db),
         current_user: Payload = Depends(get_current_user_from_cookie)):
-    # If user_id matches user_id in JWT cookie, then return user
-    if user_id == current_user.user_id:
-        result = await db.execute(select(User).where(user_id == User.user_id))
-        user = result.scalars().first()
-        if user is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Cannot find the user")
-        return user
-    else:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Wrong credentials")
+    result = await db.execute(select(User).where(current_user.user_id == User.user_id))
+    user = result.scalars().first()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Cannot find the user")
+    return user
 
 @router.post("/register", response_model=UserCreateResponse, status_code=status.HTTP_201_CREATED, tags=["user"])
 async def create_user(user_request: UserCreateRequest, background_tasks: BackgroundTasks, db: AsyncSession = Depends(get_db)): # CREATE USER
